a177fa84e48c46bda33c0899327d94d86bddbb2b
hecronk <1damiraminov@gmail.com>
2022-01-29 21:16:08 +0500

    config added, .gitignore updated, requirements.txt updated

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__name__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config(object):
+    HOST = os.environ.get('HOST')
+    DEBUG = True if os.environ.get('FLASK_ENV') == 'development' else False
