34f2f17974170534801f5d94348d9ee946a1160b
hecronk <1damiraminov@gmail.com>
2022-01-29 21:32:25 +0500

    added port to config.py

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     HOST = os.environ.get('HOST')
+    PORT = os.environ.get('PORT')
     DEBUG = True if os.environ.get('FLASK_ENV') == 'development' else False
