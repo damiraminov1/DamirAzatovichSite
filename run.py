ecae2a6e8a79bf3b0cf35a65ef75a802159e0793
hecronk <1damiraminov@gmail.com>
2022-01-30 03:56:40 +0500

    refactoring

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import create_app
 from config import Config
 
 app = create_app()
-config = {'host': Config.HOST, 'debug': Config.DEBUG, 'port': Config.PORT}
+config = {'host': Config.HOST, 'port': Config.PORT, 'debug': Config.DEBUG}
 
 if __name__ == '__main__':
     app.run(**config)
