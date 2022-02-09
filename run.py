f66c8e22b69855983f81e0f871fe0f8f3baa579d
hecronk <1damiraminov@gmail.com>
2022-02-09 17:40:43 +0500

    NGINX added, HTTPS added

--- run.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-from config import Config
-
-app = create_app()
-config = {'host': Config.HOST, 'port': Config.PORT, 'debug': Config.DEBUG}
-
-if __name__ == '__main__':
-    app.run(**config)
