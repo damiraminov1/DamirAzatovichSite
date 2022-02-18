91522c6d952c69626d118927ec81a837c6352e44
hecronk <1damiraminov@gmail.com>
2022-02-18 15:16:41 +0500

    html files edited

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,9 +5,9 @@ from flask import render_template
 @bp.route('/index', methods=['GET', 'POST'])
 @bp.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('main/index.html')
+    return render_template('main/index.html', title='Главная')
 
 
 @bp.route('/projects', methods=['GET', 'POST'])
 def projects():
-    return render_template('main/projects.html')
+    return render_template('main/projects.html', title='Проекты')
